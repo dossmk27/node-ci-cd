f5f580342d1999b21198e73c29625dfce2e37f53
cfmitrah <kalidhasan.m@mitrahsoft.com>
2024-12-03 16:33:25 +0530

    new commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World! This is a Node.js app running in Docker1.\n');
+  res.end('Hello, World! This is a Node.js app running in Docker17.\n');
 });
 
 server.listen(PORT, () => {
