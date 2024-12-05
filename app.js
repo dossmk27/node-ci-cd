532acda9909dc20f46f78c5342554d0877718ae6
dossmk27 <falcon27.doss@gmail.com>
2024-12-05 22:41:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('This is Doss MK17.\n');
+  res.end('This is Doss MK17 Testing on Development.\n');
 });
 
 server.listen(PORT, () => {
